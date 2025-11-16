91d87f932c8f074490709cb18df76996f8534d70
Asad2114011 <asad2114011@gmail.com>
2025-11-16 12:35:23 +0600

    create route file

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:mynotes/constants/routes.dart';
 import 'package:mynotes/firebase_options.dart';
 import 'package:mynotes/views/login_view.dart';
 import 'package:mynotes/views/register_view.dart';
@@ -16,9 +17,9 @@ void main() {
       theme: ThemeData(primarySwatch: Colors.blue),
       home: const Homepage(),
       routes: {
-        '/login/':(context)=>const LoginView(),
-        '/register/':(context)=>const RegisterView(),
-        '/notes/':(context)=>const NoteView(),
+        loginRoute:(context)=>const LoginView(),
+        registerRoute:(context)=>const RegisterView(),
+        noteRoute:(context)=>const NoteView(),
       },
     ),
   );
@@ -39,6 +40,7 @@ class Homepage extends StatelessWidget {
             case ConnectionState.done:
             final user=FirebaseAuth.instance.currentUser;
             if(user!=null){
+             // return NoteView();
               if(user.emailVerified){
                 return const NoteView();
               }
@@ -79,7 +81,7 @@ class _NoteViewState extends State<NoteView> {
                 final shouldLogout= await showLogOutDialog(context);
                 if(shouldLogout){
                   await FirebaseAuth.instance.signOut();
-                  Navigator.of(context).pushNamedAndRemoveUntil('/login/',(_)=>false);
+                  Navigator.of(context).pushNamedAndRemoveUntil(loginRoute,(_)=>false);
                 }
             }
           },
--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -2,6 +2,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'dart:developer' as devtools show log;
 
+import 'package:mynotes/constants/routes.dart';
+
 class LoginView extends StatefulWidget {
   const LoginView({Key? key}) : super(key: key);
 
@@ -57,7 +59,7 @@ class LoginView extends StatefulWidget {
                   await FirebaseAuth.instance
                     .signInWithEmailAndPassword(email: email, password: password);
       
-                 Navigator.of(context).pushNamedAndRemoveUntil('/notes/',(route)=>false,);
+                 Navigator.of(context).pushNamedAndRemoveUntil(noteRoute,(route)=>false,);
 
               } on FirebaseAuthException catch (e) {
                 if (e.code == 'invalid-credential') {
@@ -70,7 +72,7 @@ class LoginView extends StatefulWidget {
             child: const Text('Login'),
           ),
           TextButton(onPressed: (){
-            Navigator.of(context).pushNamedAndRemoveUntil('/register/', (route)=>false,);
+            Navigator.of(context).pushNamedAndRemoveUntil(registerRoute, (route)=>false,);
           },
           child: const Text('Not registered yet?Register here!'),),
         ],
